a5efcd7bcb5ab0d1a0a88bca1223c42bb0faece4
Shabatin-Anton <antonshabatin1993@gmail.com>
2020-06-20 00:58:37 +0300

    ver 1.1

--- src/main/java/com/homework2/Main.java
+++ src/main/java/com/homework2/Main.java
@@ -11,33 +11,33 @@ public class Main {
 
     public static void first() {
         int[] myArray = new int[20];
-        for (int i=0; i<myArray.length;i++){
+        for (int i = 0; i < myArray.length; i++) {
             myArray[i] = (int) (Math.random() * 14);
         }
         int min = myArray[0];
-        for (int j=0; j<myArray.length; j++){
-            if (min>myArray[j]){
+        for (int j = 0; j < myArray.length; j++) {
+            if (min > myArray[j]) {
                 min = myArray[j];
             }
         }
-        System.out.println("Min element of array : "+min);
+        System.out.println("Min element of array : " + min);
 
         int max = myArray[0];
-        for (int j=0; j<myArray.length; j++){
-            if (max<myArray[j]){
+        for (int j = 0; j < myArray.length; j++) {
+            if (max < myArray[j]) {
                 max = myArray[j];
             }
         }
-        System.out.println("Max element of array : "+max);
+        System.out.println("Max element of array : " + max);
 
         Arrays.sort(myArray);
 
-        for(int i = myArray.length-1 ; i > 0 ; i--){
-            for(int j = 0 ; j < i ; j++){
-                if( myArray[j] < myArray[j+1] ){
+        for (int i = myArray.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (myArray[j] < myArray[j + 1]) {
                     int x = myArray[j];
-                    myArray[j] = myArray[j+1];
-                    myArray[j+1] = x;
+                    myArray[j] = myArray[j + 1];
+                    myArray[j + 1] = x;
                 }
             }
         }
@@ -49,15 +49,15 @@ public class Main {
         System.out.println("Sum of array elements : " + sum);
 
         for (int i = 0; i < myArray.length; i++) {
-            myArray[i] = (int) Math.pow(myArray[i],2);
+            myArray[i] = (int) Math.pow(myArray[i], 2);
         }
         for (int i = 0; i < myArray.length; i++) {
             System.out.println(myArray[i]);
         }
     }
 
-    public static void second(){
-        String[][] matrix ={
+    public static void second() {
+        String[][] matrix = {
                 {"that", "tree", "blue"},
                 {"ice", "i", "see"},
                 {"water", "human", "want"}
@@ -75,11 +75,11 @@ public class Main {
         }
     }
 
-    public static void third(){
-        int [] zero = {3,2,3,1,4,2,8,3};
+    public static void third() {
+        int[] zero = {3, 2, 3, 1, 4, 2, 8, 3};
         for (int i = 0; i < zero.length; i++) {
             for (int j = i + 1; j < zero.length; j++) {
-                if (zero[i] == zero[j]){
+                if (zero[i] == zero[j]) {
                     zero[j] = 0;
                 }
             }
