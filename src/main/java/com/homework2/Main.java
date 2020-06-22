d61408022fe562c9e308f1e79618dfd47f6f9d9b
Shabatin-Anton <antonshabatin1993@gmail.com>
2020-06-22 20:35:18 +0300

    ver 1.2

--- src/main/java/com/homework2/Main.java
+++ src/main/java/com/homework2/Main.java
@@ -4,34 +4,77 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        first();
-        second();
-        third();
+
+        int[] myArray = new int[20];
+
+        random(myArray);
+        minOfArray(myArray);
+        maxOfArray(myArray);
+        sortAscending(myArray);
+        sortDescending(myArray);
+        additionOfArrElements(myArray);
+        arraySquaring(myArray);
+
+        String[][] matrix = {
+                {"that", "tree", "blue"},
+                {"ice", "i", "see"},
+                {"water", "human", "want"}
+        };
+        diagonal(matrix);
+
+        int[] zero = {3, 2, 3, 1, 4, 2, 8, 3};
+        duplicateToZero(zero);
     }
 
-    public static void first() {
-        int[] myArray = new int[20];
+    public static void random(int[] myArray) {
         for (int i = 0; i < myArray.length; i++) {
-            myArray[i] = (int) (Math.random() * 14);
+            myArray[i] = (int) (Math.random() * 34);
         }
+        printArray(myArray);
+    }
+
+    public static void minOfArray(int[] myArray) {
         int min = myArray[0];
         for (int j = 0; j < myArray.length; j++) {
             if (min > myArray[j]) {
                 min = myArray[j];
             }
         }
-        System.out.println("Min element of array : " + min);
+        System.out.println("Min of array is : " + min);
+    }
 
+    public static void maxOfArray(int[] myArray) {
         int max = myArray[0];
         for (int j = 0; j < myArray.length; j++) {
             if (max < myArray[j]) {
                 max = myArray[j];
             }
         }
-        System.out.println("Max element of array : " + max);
+        System.out.println("Max of array is : " + max);
+    }
 
-        Arrays.sort(myArray);
+    public static void additionOfArrElements(int[] myArray) {
+        int add = 0;
+        for (int i = 0; i < myArray.length; i++) {
+            add = add + myArray[i];
+        }
+        System.out.println("Addition of array elements : " + add);
+    }
 
+    public static void sortAscending(int[] myArray) {
+        for (int i = myArray.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (myArray[j] > myArray[j + 1]) {
+                    int x = myArray[j];
+                    myArray[j] = myArray[j + 1];
+                    myArray[j + 1] = x;
+                }
+            }
+        }
+        printArray(myArray);
+    }
+
+    public static void sortDescending(int[] myArray) {
         for (int i = myArray.length - 1; i > 0; i--) {
             for (int j = 0; j < i; j++) {
                 if (myArray[j] < myArray[j + 1]) {
@@ -41,42 +84,25 @@ public class Main {
                 }
             }
         }
+        printArray(myArray);
+    }
 
-        int sum = 0;
-        for (int i = 0; i < myArray.length; i++) {
-            sum = sum + myArray[i];
-        }
-        System.out.println("Sum of array elements : " + sum);
-
+    public static void arraySquaring(int[] myArray) {
         for (int i = 0; i < myArray.length; i++) {
             myArray[i] = (int) Math.pow(myArray[i], 2);
         }
-        for (int i = 0; i < myArray.length; i++) {
-            System.out.println(myArray[i]);
-        }
+        printArray(myArray);
     }
 
-    public static void second() {
-        String[][] matrix = {
-                {"that", "tree", "blue"},
-                {"ice", "i", "see"},
-                {"water", "human", "want"}
-        };
-        String[] diagonal = new String[matrix.length];
-
+    public static void diagonal(String[][] matrix) {
+        System.out.print("Diagonal is : ");
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                diagonal[i] = matrix[i][j];
-                i++;
-            }
+            System.out.print(matrix[i][i] + " ");
         }
-        for (int i = 0; i < diagonal.length; i++) {
-            System.out.print(diagonal[i] + " ");
-        }
+        System.out.println();
     }
 
-    public static void third() {
-        int[] zero = {3, 2, 3, 1, 4, 2, 8, 3};
+    public static void duplicateToZero(int[] zero) {
         for (int i = 0; i < zero.length; i++) {
             for (int j = i + 1; j < zero.length; j++) {
                 if (zero[i] == zero[j]) {
@@ -84,5 +110,13 @@ public class Main {
                 }
             }
         }
+        printArray(zero);
+    }
+
+    public static void printArray(int[] myArray) {
+        for (int i = 0; i < myArray.length; i++) {
+            System.out.print(myArray[i] + " ");
+        }
+        System.out.println();
     }
 }
